6af927ecdbdb1709f4599cb0de625aa6f05a72dc
Javkob <javkob.witness@gmail.com>
2022-11-11 23:32:13 +0100

    Styling with Angular Material

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,7 +5,9 @@ import { HeaderComponent } from './components/header/header.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { RouterModule } from '@angular/router';
 import { FlexModule } from '@angular/flex-layout';
-
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatListModule} from '@angular/material/list';
 
 
 @NgModule({
@@ -18,6 +20,9 @@ import { FlexModule } from '@angular/flex-layout';
     CommonModule,
     RouterModule,
     FlexModule,
+    MatIconModule,
+    MatButtonModule,
+    MatListModule
   ],
   exports: [
     FooterComponent,
