d998fa60424fcbea5ffac99e5db859bee1638f18
Javkob <javkob.witness@gmail.com>
2022-11-16 23:28:35 +0100

    Mackup Products

--- src/app/modules/product/product.component.ts
+++ src/app/modules/product/product.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from './model/product';
+import { ProductService } from './product.service';
 
+interface product {
+  name: "Produkt 1",
+  category: "Kategoria 1",
+  description: "Opis produktu 1",
+  price: 11.99,
+  currency: "PLN"
+}
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss']
 })
 export class ProductComponent implements OnInit {
+  products: Product[]= [];
 
-  constructor() { }
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts(){
+    this.products = this.productService.getProducts();
   }
 
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,9 +5,9 @@ import { HeaderComponent } from './components/header/header.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { RouterModule } from '@angular/router';
 import { FlexModule } from '@angular/flex-layout';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatListModule} from '@angular/material/list';
+import { MaterialModule } from './material.module';
+
+
 
 
 @NgModule({
@@ -20,14 +20,13 @@ import {MatListModule} from '@angular/material/list';
     CommonModule,
     RouterModule,
     FlexModule,
-    MatIconModule,
-    MatButtonModule,
-    MatListModule
+    MaterialModule
   ],
   exports: [
     FooterComponent,
     HeaderComponent,
-    SidebarComponent
+    SidebarComponent,
+    MaterialModule
   ]
 })
 export class SharedModule { }
