3f75a44e8282145e5dfc92f4db52961b450dd8fc
Javkob <javkob.witness@gmail.com>
2023-03-13 21:23:53 +0100

    Implemented displaying reviews with product details and adding a form to add reviews to a product

--- src/app/layouts/default/default.module.ts
+++ src/app/layouts/default/default.module.ts
@@ -8,6 +8,7 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { SharedModule } from 'src/app/shared/shared.module';
 import {ProductDetailsComponent} from "../../modules/product-details/product-details.component";
 import {CategoryComponent} from "../../modules/category/category.component";
+import {ReactiveFormsModule} from "@angular/forms";
 
 
 
@@ -23,7 +24,8 @@ import {CategoryComponent} from "../../modules/category/category.component";
     CommonModule,
     RouterModule,
     FlexLayoutModule,
-    SharedModule
+    SharedModule,
+    ReactiveFormsModule
   ]
 })
 export class DefaultModule { }
